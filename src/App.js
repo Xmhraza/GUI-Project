a8c4f59ce4a37ccc95d5586329e6ccc065966475
Xmhraza <xmhraza123@gmail.com>
2022-03-21 22:44:56 +0000

    master

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
           setQuery('');
           console.log(result);
 
-          fetch(`${api1.base}onecall?lat=${result.coord.lat}&lon=${result.coord.lon}&APPID=${api1.key}`)
+          fetch(`${api1.base}onecall?lat=${result.coord.lat}&lon=${result.coord.lon}&units=metric&APPID=${api1.key}`)
           .then(res => res.json())
           .then(result => {
           setWeather1(result);
@@ -175,16 +175,83 @@ function App() {
 
       {(typeof weather.main != "undefined") ? (
       <main className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'color hot' : 'color warm') : 'color' }>
-       <button className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'button hot' : 'button warm') : 'button' }  type="button" 
+       <button className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'secondButton hot' : 'secondButton warm') : 'secondButton' }  type="button" 
           onClick={checkTrue}><p>Today</p>
-          </button> 
-        <div>
-          {weather1.daily[0].temp.day}
-          {weather.main.temp}
+          </button>
+
+
+       <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime hot' : 'iconTime warm') : 'iconTime' }>
+          <div className='data'>{new Date(weather1.hourly[0].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[0].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[0].weather[0].icon + ".png"} alt="Weather icon"/>
         </div>
-        <div>
-        {new Date(weather1.hourly[0].dt*1000).getHours().toLocaleString()}:00
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime2 hot' : 'iconTime2 warm') : 'iconTime2' }>
+          <div className='data'>{new Date(weather1.hourly[1].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[1].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[1].weather[0].icon + ".png"} alt="Weather icon"/>
         </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime hot' : 'iconTime warm') : 'iconTime' }>
+          <div className='data'>{new Date(weather1.hourly[2].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[2].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[2].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime2 hot' : 'iconTime2 warm') : 'iconTime2' }>
+          <div className='data'>{new Date(weather1.hourly[3].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[3].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[3].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime hot' : 'iconTime warm') : 'iconTime' }>
+          <div className='data'>{new Date(weather1.hourly[4].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[4].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[4].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime2 hot' : 'iconTime2 warm') : 'iconTime2' }>
+          <div className='data'>{new Date(weather1.hourly[5].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[5].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[5].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        
+
+       <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime hot' : 'iconTime warm') : 'iconTime' }>
+          <div className='data'>{new Date(weather1.hourly[6].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[6].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[6].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime2 hot' : 'iconTime2 warm') : 'iconTime2' }>
+          <div className='data'>{new Date(weather1.hourly[7].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[7].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[7].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime hot' : 'iconTime warm') : 'iconTime' }>
+          <div className='data'>{new Date(weather1.hourly[8].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[8].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[8].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime2 hot' : 'iconTime2 warm') : 'iconTime2' }>
+          <div className='data'>{new Date(weather1.hourly[9].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[9].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[9].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime hot' : 'iconTime warm') : 'iconTime' }>
+          <div className='data'>{new Date(weather1.hourly[10].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[10].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[10].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+
+        <div className={(weather.main.temp > 12) ? ((weather.main.temp > 24) ? 'iconTime2 hot' : 'iconTime2 warm') : 'iconTime2' }>
+          <div className='data'>{new Date(weather1.hourly[11].dt*1000).getHours().toLocaleString()}:00</div>
+          <div className='tempMargin'>{Math.round(weather1.hourly[11].temp)}°c</div>
+          <img id="wicon" className='iconMargin' src={"http://openweathermap.org/img/wn/"+ weather1.hourly[11].weather[0].icon + ".png"} alt="Weather icon"/>
+        </div>
+      
      
       </main>
       ) : ('')}
